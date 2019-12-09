f3ebc43f1f4e0976b1fcf79db8df51cf3213cb2e
Olyno <alexlew95@gmail.com>
2019-12-09 03:10:24 +0100

    [master] - [add] - Added brand logo in Navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -9,6 +9,9 @@
 
 <div class="navbar is-yellow is-fixed-top">
 	<div class="navbar-brand">
+		<a class="navbar-item" href="/skript-website">
+			<img alt="Skript logo" src="favicon.png" width="55" height="30">
+		</a>
 		<a href="#!" on:click|preventDefault={() => active = !active} class="navbar-burger burger {active ? "is-active": ""}">
 			<span></span>
 			<span></span>
@@ -16,7 +19,6 @@
 		</a>
 	</div>
 	<div class="navbar-menu {active ? "is-active" : ""}">
-		<a href="/skript-website" class="navbar-item">HOME</a>
 		<div class="navbar-item has-dropdown is-hoverable">
 			<a href="documentation" on:click|preventDefault class="navbar-link">DOCUMENTATION</a>
 			<div class="navbar-dropdown">
